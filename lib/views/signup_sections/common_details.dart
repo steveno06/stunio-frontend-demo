9ae0364d9954f7be64fc2e85c7e5010889d38580
steveno06 <stevenohrdorf@gmail.com>
2024-12-23 00:33:16 +0100

    refactor: common details to reusable widgets

--- lib/views/signup_sections/common_details.dart
+++ lib/views/signup_sections/common_details.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:stunio_frontend/viewmodels/signup_viewmodel.dart';
+import 'package:stunio_frontend/views/components/header_2.dart';
+import 'package:stunio_frontend/views/components/simple_button.dart';
+import 'package:stunio_frontend/views/components/simple_text_field.dart';
 
 class CommonDetails extends StatefulWidget {
   final SignupViewmodel viewModel;
@@ -24,33 +27,32 @@ class _CommonDetailsState extends State<CommonDetails> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Lets get the essentials"),
-          TextField(
-            controller: _usernameController,
-            decoration: const InputDecoration(hintText: "username"),
-          ),
-          TextField(
-            controller: _emailController,
-            decoration: const InputDecoration(hintText: "email"),
-          ),
-          TextField(
-            controller: _passwordController,
-            decoration: InputDecoration(hintText: "password"),
-            obscureText: true,
-          ),
-          TextButton(onPressed: (){
-            widget.viewModel.setCommonData(
-              _usernameController.text,
-              _emailController.text, 
-              _passwordController.text
-            );
-            widget.viewModel.nextSection();
-          }, child: Text("Next Steps"))
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(14),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 14),
+              child: const Header2(label: "Lets get the essentials"),
+            ),
+            SimpleTextField(fieldName: "Username", inputController: _usernameController),
+            SimpleTextField(fieldName: "Email", inputController: _emailController),
+            SimpleTextField(fieldName: "Password", inputController: _passwordController, isObscure: true),
+            Padding(
+              padding: const EdgeInsets.only(top: 14),
+              child: SimpleButton(onPressed: (){
+                widget.viewModel.setCommonData(
+                  _usernameController.text,
+                  _emailController.text, 
+                  _passwordController.text
+                );
+                widget.viewModel.nextSection();
+              }, label: "Next Steps"),
+            )
+          ],
+        ),
       ),
     );
   }
