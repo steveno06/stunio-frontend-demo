567749c3c6b709328a372ead69e69498d26701ba
steveno06 <stevenohrdorf@gmail.com>
2024-12-23 00:41:09 +0100

    refactor: business sign up uses shared widgets

--- lib/views/signup_sections/business_details.dart
+++ lib/views/signup_sections/business_details.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:stunio_frontend/viewmodels/signup_viewmodel.dart';
+import 'package:stunio_frontend/views/components/header_2.dart';
+import 'package:stunio_frontend/views/components/simple_button.dart';
+import 'package:stunio_frontend/views/components/simple_text_field.dart';
 
 import '../business_homepage.dart';
 
@@ -26,39 +29,39 @@ class _BusinessDetailsState extends State<BusinessDetails> {
   }
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Tell us more about your business!"),
-          TextField(
-            controller: _companyNameController,
-            decoration: InputDecoration(hintText: "company name"),
-          ),
-          TextField(
-            controller: _industryController,
-            decoration: InputDecoration(hintText: "industry"),
-          ),
-          TextField(
-            controller: _companySizeController,
-            decoration: InputDecoration(hintText: "company size"),
-          ),
-          TextButton(onPressed: () async {
-            widget.viewModel.setBusinessData(
-              _companyNameController.text, 
-              _industryController.text, 
-              int.parse(_companySizeController.text
-            ));
-            await widget.viewModel.register().then((success){
-              if(success){
-                Navigator.pushReplacement(
-                  context, 
-                  MaterialPageRoute(builder: (context) => BusinessHomepage(userId: widget.viewModel.userId!, userType: widget.viewModel.userTypeResponse!))
-                );
-              }
-            });
-          }, child: Text("Create Business Account"))
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(14),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 14),
+              child: Header2(label: "Tell us more about your business!"),
+            ),
+            SimpleTextField(fieldName: "Company Name", inputController: _companyNameController),
+            SimpleTextField(fieldName: "Industry", inputController: _industryController),
+            SimpleTextField(fieldName: "Company Size", inputController: _companySizeController),
+            Padding(
+              padding: const EdgeInsets.only(top: 14),
+              child: SimpleButton(onPressed: () async {
+                widget.viewModel.setBusinessData(
+                  _companyNameController.text, 
+                  _industryController.text, 
+                  int.parse(_companySizeController.text
+                ));
+                await widget.viewModel.register().then((success){
+                  if(success){
+                    Navigator.pushReplacement(
+                      context, 
+                      MaterialPageRoute(builder: (context) => BusinessHomepage(userId: widget.viewModel.userId!, userType: widget.viewModel.userTypeResponse!))
+                    );
+                  }
+                });
+              }, label: "Create Business Account"),
+            )
+          ],
+        ),
       ),
     );
   }
